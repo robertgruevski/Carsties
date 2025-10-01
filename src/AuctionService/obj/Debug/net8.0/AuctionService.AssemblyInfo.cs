10e789e49cd1410df5caa304b6368c080857580d
robertgruevski <robertgruevski@outlook.com>
2025-10-01 15:40:35 -0400

    End of section 4

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3f9b01c74033d67f34b76ecd0a759cb1a9307d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9b5821335dc40338b098b7cab816dee7a617f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
