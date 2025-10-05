347e6b24259ae669ea2841d1450ead49d26f9692
robertgruevski <robertgruevski@outlook.com>
2025-10-05 18:33:37 -0400

    End of section 9

--- frontend/web-app/app/components/EmptyFilter.tsx
+++ frontend/web-app/app/components/EmptyFilter.tsx
@@ -1,18 +1,25 @@
+"use client";
+
 import { useParamsStore } from "@/hooks/useParamsStore";
 import React from "react";
 import Heading from "./Heading";
 import { Button } from "flowbite-react";
+import { signIn } from "next-auth/react";
 
 type Props = {
 	title?: string;
 	subtitle?: string;
 	showReset?: boolean;
+	showLogin?: boolean;
+	callbackUrl?: string;
 };
 
 export default function EmptyFilter({
 	title = "No matches for this filter",
 	subtitle = "Try changing the filter or search term",
 	showReset,
+	showLogin,
+	callbackUrl,
 }: Props) {
 	const reset = useParamsStore((state) => state.reset);
 
@@ -23,6 +30,13 @@ export default function EmptyFilter({
 				{showReset && (
 					<Button outline onClick={reset}>
 						Remove filters
+					</Button>
+				)}
+				{showLogin && (
+					<Button
+						outline
+						onClick={() => signIn("id-server", { redirectTo: callbackUrl })}>
+						Login
 					</Button>
 				)}
 			</div>
--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf16fbafca11ae29aa011f9575e104f396e3cee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a51ac8da4312a17bcc35c6ec7ad6e3084d7d7ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
